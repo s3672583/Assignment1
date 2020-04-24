d45beb6aeae27d2c699ed61611cb6cef18795387
Varun Gupta <s3672583@student.rmit.edu.au>
2020-04-24 17:47:03 +0530

    Trying failing

--- src/test/unit/index.test.js
+++ src/test/unit/index.test.js
@@ -10,6 +10,6 @@ describe('models/index', function () {
 
   it('returns the user model', function () {
     var models = require('../../models');
-    expect(models.Users).to.be.ok();
+    expect(models.Users).to.be.not.ok();
   });
 });
